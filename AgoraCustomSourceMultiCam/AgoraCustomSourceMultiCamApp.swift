9b544ae5cb78094a122565f3be3701374bcfe53b
bac1314 <bacchenghuang@gmail.com>
2025-09-02 17:06:57 +0800

    Initial Commit

---
+++ AgoraCustomSourceMultiCam/AgoraCustomSourceMultiCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgoraCustomSourceMultiCamApp.swift
+//  AgoraCustomSourceMultiCam
+//
+//  Created by Bac Huang on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct AgoraCustomSourceMultiCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
